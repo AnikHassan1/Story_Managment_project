459e169d98dc5945cd90964a2a567c83e45920c6
Anik Hassan1 <ah491606@gmail.com>
2024-01-02 10:51:55 +0600

    Complete project

--- report.php
+++ report.php
@@ -16,84 +16,116 @@ while ($arr = $QUE->fetch_assoc()) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Report</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 
 <body>
-    <form action="report.php" method="POST">
 
-        Select Product :
-        <select name="Product_name" id="">
-            <?php
-            $sql = "SELECT * FROM product";
-            $datas =  $conn->query($sql);
+</body>
 
-            while ($data = $datas->fetch_assoc()) {
-                $id = $data['Product_id'];
-                $name = $data['Product_name'];
+<body>
+    <div class="container bg-light">
+
+        <div class="container-foulid border-bottom border-success "> <!--topbar-->
+
+            <?php require("topbar.php") ?>
 
 
-            ?>
+        </div><!--end-- topbar-->
+        <div class="container-foulid">
+            <div class="row">
+                <div class="col-sm-3 bg-light p-0 m-0 border-end border-success">
 
-                <option value="<?php echo $id ?>"><?php echo  $name ?></option>
-            <?php } ?>
-        </select>
+                    <?php require("slidebar.php") ?>
+                </div><!--end of left -->
+                <div class="col-sm-9 pt-4 text-success">
+                    <form action="report.php" method="POST">
 
-        <input type="submit" value="Generate Report" name="submit">
-    </form>
-    <h1>Store Product</h1>
-    <?php
-    if (isset($_POST['Product_name'])) {
-        $product_name = $_POST['Product_name'];
+                        Select Product :
+                        <select name="Product_name" id="">
+                            <?php
+                            $sql = "SELECT * FROM product";
+                            $datas =  $conn->query($sql);
 
-        $sql = "SELECT * FROM store_produt WHERE story_product_name=$product_name";
-        $query = $conn->query($sql);
-        while ($DATA = $query->fetch_assoc()) {
-
-            $storeName = $DATA['story_product_name'];
-            $storequan =   $DATA['Story_product_quantity'];
-            $storedate =    $DATA['story_product_date'];
-
-            echo "<h3>$namearrey[$storeName]</h3>";
-            echo "<table border='1'>
-            <tr>
-                <th>store Date</th>
-                <th>Amount</th>
-            </tr>";
-            echo "<tr>
-            <td> $storequan</td>
-            <td>$storedate</td>
-           </tr>";
-            echo  "</table>";
-        }
-    }
-    ?>
-    <h1>Spend Product</h1>
-    <?php
-    if (isset($_POST['Product_name'])) {
-        $product_name = $_POST['Product_name'];
-
-        $sql2 = "SELECT * FROM spend_product WHERE Spend_product_name=$product_name";
-        $query2 = $conn->query($sql2);
-        while ($datas = $query2->fetch_assoc()) {
+                            while ($data = $datas->fetch_assoc()) {
+                                $id = $data['Product_id'];
+                                $name = $data['Product_name'];
 
 
-            $spendquan =   $datas['Spend_product_quantity'];
-            $spenddate =   $datas['Spend_product_date'];
-            $spendName =   $datas['Spend_product_name'];
-            echo "<h3>$namearrey[$spendName]</h3>";
-            echo "<table border='1'>
-            <tr>
-                <th>spend Date</th>
-                <th>Amount</th>
-            </tr>";
-            echo "<tr>
-            <td>$spendquan</td>
-            <td> $spenddate</td>
-           </tr>";
-            echo  "</table>";
-        }
-    }
-    ?>
+                            ?>
+
+                                <option value="<?php echo $id ?>"><?php echo  $name ?></option>
+                            <?php } ?>
+                        </select>
+
+                        <input type="submit" value="Generate Report" name="submit" class="text-success">
+                    </form>
+                    <h1>Store Product</h1>
+                    <?php
+                    if (isset($_POST['Product_name'])) {
+                        $product_name = $_POST['Product_name'];
+
+                        $sql = "SELECT * FROM store_produt WHERE story_product_name=$product_name";
+                        $query = $conn->query($sql);
+                        while ($DATA = $query->fetch_assoc()) {
+
+                            $storeName = $DATA['story_product_name'];
+                            $storequan =   $DATA['Story_product_quantity'];
+                            $storedate =    $DATA['story_product_date'];
+
+                            echo "<h3>$namearrey[$storeName]</h3>";
+                            echo "<table class='table table-success table-striped'>
+                                    <tr>
+                                        <th>store Date</th>
+                                        <th>Amount</th>
+                                    </tr>";
+                            echo "<tr>
+                                    <td> $storequan</td>
+                                    <td>$storedate</td>
+                                </tr>";
+                        }
+                        echo  "</table>";
+                    }
+                    ?>
+                    <h1>Spend Product</h1>
+                    <?php
+                    if (isset($_POST['Product_name'])) {
+                        $product_name = $_POST['Product_name'];
+
+                        $sql2 = "SELECT * FROM spend_product WHERE Spend_product_name=$product_name";
+                        $query2 = $conn->query($sql2);
+                        while ($datas = $query2->fetch_assoc()) {
+
+
+                            $spendquan =   $datas['Spend_product_quantity'];
+                            $spenddate =   $datas['Spend_product_date'];
+                            $spendName =   $datas['Spend_product_name'];
+                            echo "<h3> Category name : $namearrey[$spendName]</h3>";
+                            echo "<table class='table table-success table-striped'>
+                                    <tr>
+                                        <th>spend Date</th>
+                                        <th>Quentity</th>
+                                    </tr>";
+                            echo "<tr>
+                                     <td> $spenddate</td>
+                                    <td>$spendquan</td>
+                                    
+                                </tr>";
+                            echo  "</table>";
+                        }
+                    }
+                    ?>
+                </div><!--end of right -->
+            </div><!--end of row -->
+        </div>
+        <div class="container-foulid border-top 
+           border-success mt-2">
+            <?php require('footer.php') ?>
+        </div>
+    </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 
 </html>
